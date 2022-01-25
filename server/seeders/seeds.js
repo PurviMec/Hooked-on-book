0d9260439e584a7b6c1b5a56a5c0229909e8efd9
jeffmlb91 <jeffmlb91@gmail.com>
2022-01-25 13:18:46 -0500

    update seeds.js and dateFormat.js "

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,46 +1,60 @@
-const faker = require("faker");
-
-const db = require("../config/connection");
-const { User, Book } = require("../models");
-
-db.once("open", async () => {
+const faker = require(“faker”);
+const db = require(“../config/connection”);
+const { User, Book } = require(“../models”);
+db.once(“open”, async () => {
   await Book.deleteMany({});
   await User.deleteMany({});
-
   // create user data
   const userData = [];
-
   for (let i = 0; i < 50; i += 1) {
     const username = faker.internet.userName();
     const email = faker.internet.email(username);
     const password = faker.internet.password();
-
     userData.push({ username, email, password });
   }
-
-  // let createdBooks = [];
-
-  //     for (let i = 0; i < 100; i += 1) {
-  //     const bookTitles = faker.lorem.words(Math.round(Math.random() * 20) + 1);
-
-  //     const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-  //     const { username, _id: userId } = createdUsers.ops[randomUserIndex];
-
-  //     const createdBooks = await Book.create({ bookTitles, username });
-
-  //     const updatedUser = await User.updateOne(
-  //       { _id: userId },
-  //       { $push: { createdBooks: createdThought._id } }
-  //     );
-
-  //     createdThoughts.push(createdBooks);
-  //   }
+  const createdUsers = await User.collection.insertMany(userData);
+   let createdBooks = [];
+      for (let i = 0; i < 100; i += 1) {
+      const name = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+      const description = faker.lorem.words(Math.round(Math.random() * 40) + 1);
+      // const genere = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+      const author = faker.lorem.words(Math.round(Math.random() * 15) + 1);
+      const rent = faker.lorem.num(Math.round(Math.random() * 5) + 1);
+      const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+      const { username, _id: userId } = createdUsers.ops[randomUserIndex];
+      const createdBook = await Book.create({ name, description, author, username, rent});
+      const updatedUser = await User.updateOne(
+        { _id: userId },
+        { $push: { booklist : createdBook._id } }
+      );
+      createdBooks.push(createdBook);
+    }
+    //create reactions
+    for (let i = 0; i < 100; i += 1) {
+      const reactionBody = faker.lorem.words(Math.round(Math.random() * 40) + 1);
+      const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+      const { username } = createdUsers.ops[randomUserIndex];
+      const randomBookIndex = Math.floor(Math.random() * createdBooks.length);
+      const { _id: bookId } = createdBooks[randomBookIndex];
+      await Book.updateOne(
+        { _id: bookId },
+        { $push: { reactions: { reactionBody, username } } },
+        { runValidators: true }
+      );
+    }
+    // create genere
+    for (let i = 0; i < 100; i += 1) {
+      const genereBody = faker.lorem.words(Math.round(Math.random() * 15) + 1);
+      // const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+      // const { username } = createdUsers.ops[randomUserIndex];
+      const randomBookIndex = Math.floor(Math.random() * createdBooks.length);
+      const { _id: bookId } = createdBooks[randomBookIndex];
+      await Book.updateOne(
+        { _id: bookId },
+        { $push: { genere: { genereBody } } },
+        { runValidators: true }
+      );
+    }
+    console.log(‘all done!’);
+    process.exit(0);
 });
-
-
-
-
-
-
-
-
